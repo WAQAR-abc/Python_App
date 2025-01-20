91738cc9a0ff2cd94a0e42f301b547a902dc8dc7
WAQAR SHAIKH <127889054+WAQAR-abc@users.noreply.github.com>
2025-01-20 16:40:17 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import logging
+
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # Log the request with an info level message
+    app.logger.info('App endpoint was accessed.')
+    return 'Hello, DevOps Learners!'
+
+if __name__ == '__main__':
+    # This ensures the app listens on all network interfaces inside the container
+    app.run(host='0.0.0.0', port=5000)
+
